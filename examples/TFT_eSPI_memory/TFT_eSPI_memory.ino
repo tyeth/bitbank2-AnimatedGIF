8dce82befc78a3da2fa5c15c57b8b0f35449f1d6
Tyeth Gundry <tyethgundry@googlemail.com>
2022-10-09 22:31:18 +0100

    Add corporate logo example for Good-Enough.technology

--- examples/TFT_eSPI_memory/TFT_eSPI_memory.ino
+++ examples/TFT_eSPI_memory/TFT_eSPI_memory.ino
@@ -9,6 +9,8 @@
 // Adapted by Bodmer for the TFT_eSPI Arduino library:
 // https://github.com/Bodmer/TFT_eSPI
 //
+// Cut to pieces/Modified by Tyeth Gundry 
+//
 // To display a GIF from memory, a single callback function
 // must be provided - GIFDRAW
 // This function is called after each scan line is decoded
@@ -22,25 +24,17 @@
 
 //#define USE_DMA       // ESP32 ~1.25x single frame rendering performance boost for badgers.h
                         // Note: Do not use SPI DMA if reading GIF images from SPI SD card on same bus as TFT
-  #define NORMAL_SPEED  // Comment out for rame rate for render speed test
 
 // Load GIF library
 #include <AnimatedGIF.h>
 AnimatedGIF gif;
 
-// Example AnimatedGIF library images
-#include "../test_images/badgers.h"
-#include "../test_images/homer.h"
-#include "../test_images/homer_tiny.h"
-#include "../test_images/pattern.h"
+#include "good-enough-final.h"
+
 
                                 // ESP32 40MHz SPI single frame rendering performance
                                 // Note: no DMA performance gain on smaller images or transparent pixel GIFs
-  #define GIF_IMAGE ucBadgers   //  No DMA  63 fps, DMA:  71fps
-//#define GIF_IMAGE ucHomer     //  No DMA 162 fps, DMA: 141 fps
-//#define GIF_IMAGE homer_tiny  //  No DMA 564 fps, DMA: 481 fps
-//#define GIF_IMAGE ucPattern   //  No DMA  90 fps, DMA:  78 fps
-
+  #define GIF_IMAGE good_enough_final
 #include <SPI.h>
 #include <TFT_eSPI.h>
 
@@ -54,12 +48,11 @@ void setup() {
   tft.initDMA();
 #endif
   tft.setRotation(1);
-  tft.fillScreen(TFT_BLACK);
+  tft.fillScreen(TFT_WHITE);
 
   gif.begin(BIG_ENDIAN_PIXELS);
 }
 
-#ifdef NORMAL_SPEED // Render at rate that is GIF controlled
 void loop()
 {
   // Put your main code here, to run repeatedly:
@@ -75,26 +68,3 @@ void loop()
     tft.endWrite(); // Release TFT chip select for other SPI devices
   }
 }
-#else // Test maximum rendering speed
-void loop()
-{
-  long lTime = micros();
-  int iFrames = 0;
-
-  if (gif.open((uint8_t *)GIF_IMAGE, sizeof(GIF_IMAGE), GIFDraw))
-  {
-    tft.startWrite(); // For DMA the TFT chip select is locked low
-    while (gif.playFrame(false, NULL))
-    {
-      // Each loop renders one frame
-      iFrames++;
-      yield();
-    }
-    gif.close();
-    tft.endWrite(); // Release TFT chip select for other SPI devices
-    lTime = micros() - lTime;
-    Serial.print(iFrames / (lTime / 1000000.0));
-    Serial.println(" fps");
-  }
-}
-#endif
